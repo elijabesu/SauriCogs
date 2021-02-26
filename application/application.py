1e968a2ffa1a82f10ef0cc26826c1c1089cbae70
saurich <ellie@saurich.com>
2021-02-26 20:54:59 +0100

    [Application] setup + cooldown reworks

--- application/application.py
+++ application/application.py
@@ -1,12 +1,11 @@
 import asyncio
 import discord
+import datetime
 
 from discord.utils import get
-from datetime import datetime, timedelta
 
 from redbot.core import Config, checks, commands
 from redbot.core.utils.predicates import MessagePredicate
-from redbot.core.utils.antispam import AntiSpam
 
 from redbot.core.bot import Red
 
@@ -18,20 +17,17 @@ class Application(commands.Cog):
     """
 
     __author__ = "saurichable"
-    __version__ = "1.2.6"
+    __version__ = "1.3.0"
 
     def __init__(self, bot: Red):
         self.bot = bot
         self.config = Config.get_conf(
             self, 5641654654621651651, force_registration=True
         )
-        self.antispam = {}
         self.config.register_guild(
-            is_set=False,
             applicant_id=None,
             accepter_id=None,
             channel_id=None,
-            applicant_role=True,
             questions=[
                 ["What position are you applying for?", "Position", 120],
                 ["What is your name?", "Name", 120],
@@ -44,15 +40,16 @@ class Application(commands.Cog):
                 ],
         )
 
+    @commands.max_concurrency(1, commands.BucketType.member, wait=True)
     @commands.command()
     @commands.guild_only()
     @checks.bot_has_permissions(manage_roles=True)
     async def apply(self, ctx: commands.Context):
         """Apply to be a staff member."""
-        if not await self.config.guild(ctx.guild).is_set():
+        if not await self.config.guild(ctx.guild).channel_id():
             return await ctx.send("Uh oh, the configuration is not correct. Ask the Admins to set it.")
 
-        if await self.config.guild(ctx.guild).applicant_role():
+        if await self.config.guild(ctx.guild).applicant_id():
             try:
                 role_add = get(ctx.guild.roles, id = await self.config.guild(ctx.guild).applicant_id())
             except TypeError:
@@ -69,12 +66,6 @@ class Application(commands.Cog):
             channel = get(ctx.guild.text_channels, name = "applications")
             if not channel:
                 return await ctx.send("Uh oh, the configuration is not correct. Ask the Admins to set it.")
-        if ctx.guild not in self.antispam:
-            self.antispam[ctx.guild] = {}
-        if ctx.author not in self.antispam[ctx.guild]:
-            self.antispam[ctx.guild][ctx.author] = AntiSpam([(timedelta(days=2), 1)])
-        if self.antispam[ctx.guild][ctx.author].spammy:
-            return await ctx.send("Uh oh, you're doing this way too frequently.")
         if not role_add:
             return await ctx.send(
                 "Uh oh. Looks like your Admins haven't added the required role."
@@ -93,7 +84,7 @@ class Application(commands.Cog):
             )
         await ctx.send(f"Okay, {ctx.author.mention}, I've sent you a DM.")
 
-        embed = discord.Embed(color=await ctx.embed_colour(), timestamp=datetime.now())
+        embed = discord.Embed(color=await ctx.embed_colour(), timestamp=datetime.datetime.now())
         embed.set_author(name="New application!", icon_url=ctx.author.avatar_url)
         embed.set_footer(
             text=f"{ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})"
@@ -128,103 +119,18 @@ class Application(commands.Cog):
         await ctx.author.send(
             "Your application has been sent to the Admins, thank you!"
         )
-        self.antispam[ctx.guild][ctx.author].stamp()
 
+    @commands.max_concurrency(1, commands.BucketType.guild, wait=True)
     @checks.admin_or_permissions(administrator=True)
-    @commands.group(autohelp=True)
+    @commands.group(autohelp=True, aliases=["setapply", "applicationset"])
     @commands.guild_only()
     @checks.bot_has_permissions(manage_channels=True, manage_roles=True)
-    async def setapply(self, ctx: commands.Context):
+    async def applyset(self, ctx: commands.Context):
         """Application settings"""
         pass
 
-    @setapply.command(name="setup")
-    async def setapply_setup(self, ctx: commands.Context):
-        """Go through the initial setup process."""
-        pred = MessagePredicate.yes_or_no(ctx)
-        role = MessagePredicate.valid_role(ctx)
-
-        applicant = get(ctx.guild.roles, name="Staff Applicant")
-        channel = get(ctx.guild.text_channels, name="applications")
-
-        await ctx.send(
-            "This will create required channel and role. Do you wish to continue? (yes/no)"
-        )
-        try:
-            await self.bot.wait_for("message", timeout=30, check=pred)
-        except asyncio.TimeoutError:
-            return await ctx.send("You took too long. Try again, please.")
-        if not pred.result:
-            return await ctx.send("Setup cancelled.")
-        if not applicant:
-            await ctx.send(
-                "Do you want applicants to gain a role? (yes/no)"
-            )
-            try:
-                await self.bot.wait_for("message", timeout=30, check=pred)
-            except asyncio.TimeoutError:
-                return await ctx.send("You took too long. Try again, please.")
-            if pred.result:
-                await self.config.guild(ctx.guild).applicant_role.set(True)
-                try:
-                    applicant = await ctx.guild.create_role(
-                        name="Staff Applicant", reason="Application cog setup"
-                    )
-                except discord.Forbidden:
-                    return await ctx.send(
-                        "Uh oh. Looks like I don't have permissions to manage roles."
-                    )
-                await self.config.guild(ctx.guild).applicant_id.set(applicant.id)
-            else:
-                await self.config.guild(ctx.guild).applicant_role.set(False)
-                await self.config.guild(ctx.guild).applicant_id.set(None)
-        if not channel:
-            await ctx.send(
-                "Do you want everyone to see the applications channel? (yes/no)"
-            )
-            try:
-                await self.bot.wait_for("message", timeout=30, check=pred)
-            except asyncio.TimeoutError:
-                return await ctx.send("You took too long. Try again, please.")
-            if pred.result:
-                overwrites = {
-                    ctx.guild.default_role: discord.PermissionOverwrite(
-                        send_messages=False
-                    ),
-                    ctx.guild.me: discord.PermissionOverwrite(send_messages=True),
-                }
-            else:
-                overwrites = {
-                    ctx.guild.default_role: discord.PermissionOverwrite(
-                        read_messages=False
-                    ),
-                    ctx.guild.me: discord.PermissionOverwrite(read_messages=True),
-                }
-            try:
-                channel = await ctx.guild.create_text_channel(
-                    "applications",
-                    overwrites=overwrites,
-                    reason="Application cog setup",
-                )
-            except discord.Forbidden:
-                return await ctx.send(
-                    "Uh oh. Looks like I don't have permissions to manage channels."
-                )
-        await ctx.send(f"What role can accept or reject applicants?")
-        try:
-            await self.bot.wait_for("message", timeout=30, check=role)
-        except asyncio.TimeoutError:
-            return await ctx.send("You took too long. Try again, please.")
-        accepter = role.result
-        await self.config.guild(ctx.guild).channel_id.set(channel.id)
-        await self.config.guild(ctx.guild).accepter_id.set(accepter.id)
-        await self.config.guild(ctx.guild).is_set.set(True)
-        await ctx.send(
-            "You have finished the setup! Please, move your new channel to the category you want it in."
-        )
-
-    @setapply.command(name="questions")
-    async def setapply_questions(self, ctx: commands.Context):
+    @applyset.command(name="questions")
+    async def applyset_questions(self, ctx: commands.Context):
         """Set custom application questions."""
         current_questions = "**Current questions:**"
         for question in await self.config.guild(ctx.guild).questions():
@@ -274,6 +180,39 @@ class Application(commands.Cog):
         await self.config.guild(ctx.guild).questions.set(list_of_questions)
         await ctx.send("Done!")
 
+    @applyset.command(name="applicant")
+    async def applyset_applicant(self, ctx: commands.Context, role: discord.Role = None):
+        """Set the Staff Applicant role.
+
+        If `role` is not provided, applicants will not get any role."""
+        if role:
+            await self.config.guild(ctx.guild).applicant_id.set(role.id)
+        else:
+            await self.config.guild(ctx.guild).applicant_id.set(None)
+        await ctx.tick()
+
+    @applyset.command(name="accepter")
+    async def applyset_accepter(self, ctx: commands.Context, role: discord.Role = None):
+        """Set the role that can accept/reject applications.
+
+        If `role` is not provided, defaults to guild administrators."""
+        if role:
+            await self.config.guild(ctx.guild).accepter_id.set(role.id)
+        else:
+            await self.config.guild(ctx.guild).accepter_id.set(None)
+        await ctx.tick()
+
+    @applyset.command(name="channel")
+    async def applyset_channel(self, ctx: commands.Context, channel: discord.TextChannel = None):
+        """Set the channel where applications will be sent.
+        
+        If `channel` is not provided, applications are disabled."""
+        if channel:
+            await self.config.guild(ctx.guild).channel_id.set(channel.id)
+        else:
+            await self.config.guild(ctx.guild).channel_id.set(None)
+        await ctx.tick()
+
     @commands.command()
     @commands.guild_only()
     @checks.bot_has_permissions(manage_roles=True)
@@ -281,7 +220,7 @@ class Application(commands.Cog):
         """Accept a staff applicant.
 
         <target> can be a mention or an ID."""
-        if not await self.config.guild(ctx.guild).is_set():
+        if not await self.config.guild(ctx.guild).channel_id():
             return await ctx.send("Uh oh, the configuration is not correct. Ask the Admins to set it.")
 
         try:
@@ -296,7 +235,7 @@ class Application(commands.Cog):
                 return await ctx.send("Uh oh, you cannot use this command.")
 
         role = MessagePredicate.valid_role(ctx)
-        if await self.config.guild(ctx.guild).applicant_role():
+        if await self.config.guild(ctx.guild).applicant_id():
             try:
                 applicant = get(ctx.guild.roles, id = await self.config.guild(ctx.guild).applicant_id())
             except TypeError:
@@ -331,7 +270,7 @@ class Application(commands.Cog):
         """Deny a staff applicant.
 
         <target> can be a mention or an ID"""
-        if not await self.config.guild(ctx.guild).is_set():
+        if not await self.config.guild(ctx.guild).channel_id():
             return await ctx.send("Uh oh, the configuration is not correct. Ask the Admins to set it.")
 
         try:
